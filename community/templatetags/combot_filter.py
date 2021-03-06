893654c15bd4657ae847d2958723a50c43f3db37
ParkSeonIk <parksimis@gmail.com>
2021-03-06 12:02:30 +0900

    Add markdown func in forum

--- community/templatetags/combot_filter.py
+++ community/templatetags/combot_filter.py
@@ -1,9 +1,17 @@
+import markdown
 from django import template
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-@register.filter
+@register.filter()
 def sub(value, arg):
     return value - arg
 
 # {{ value|sub:3}}
+
+
+@register.filter()
+def mark(value):
+    extensions = ['nl2br', 'fenced_code']
+    return mark_safe(markdown.markdown(value, extensions=extensions))
